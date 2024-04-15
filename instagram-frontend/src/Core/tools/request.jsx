d1e795a205094ffd255ec58526284b5a3fee5a22
afif-ayoubi <afifayoubi22@gmail.com>
2024-04-15 18:34:07 +0300

    ..

--- instagram-frontend/src/Core/tools/request.jsx
+++ instagram-frontend/src/Core/tools/request.jsx
@@ -1,22 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.baseURL="http://127.0.0.1:8000/api";
+axios.defaults.baseURL = "http://127.0.0.1:8000/api";
 
-export const sendRequest=async ({method,route,body})=>{
-    try{
-        const response= await axios({
-            method:method,
-            url:route,
-            data:body,
-            headers:{
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            }
-        });
-        return response;
-    }catch(error){
-        if(error.response && error.response.status==401){
-            localStorage.removeItem("token");
-        }
-        throw error;
+export const sendRequest = async ({ method, route, body }) => {
+  try {
+    const response = await axios({
+      method: method,
+      url: route,
+      data: body,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    if (error.response && error.response.status == 401) {
+      localStorage.removeItem("token");
     }
-}
+    throw error;
+  }
+};
